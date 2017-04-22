78d4ff44e469cd3307f5e480b1171dfd27fdb874
biratrai <biratrai@hotmail.com>
2017-04-21 22:36:01 -0400

    First Commit to repository

---
+++ app/src/main/java/androidthreading/gooner10/com/androidthreading/MainActivity.java
@@ -0,0 +1,13 @@
+package androidthreading.gooner10.com.androidthreading;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
