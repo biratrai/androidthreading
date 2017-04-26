baca2d6b17e05eb86f5c27dbba14d919327dcc1b
biratrai <biratrai@hotmail.com>
2017-04-25 22:58:31 -0400

    Adding class to represent file list data

---
+++ app/src/main/java/androidthreading/gooner10/com/androidthreading/FileList.java
@@ -0,0 +1,14 @@
+package androidthreading.gooner10.com.androidthreading;
+
+/**
+ * A class that represents the file to be downloaded
+ */
+
+public class FileList {
+    public static String[] files = {"file1.txt",
+            "file2.txt",
+            "file3.txt",
+            "file4.txt",
+            "file5.txt",};
+
+}
